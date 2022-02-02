f284cd390cdacb299043fcc35b900ce41ca21cbb
Evieepk12 <eviepk12@gmail.com>
2022-02-02 20:33:21 +0700

    new madlbs

--- random_madlib.py
+++ random_madlib.py
@@ -1,6 +1,6 @@
-from sample_madlibs import hewan, komputer_ajaib
+from sample_madlibs import hewan, komputer_ajaib, zeus_olympia
 import random
 
 if __name__ == "__main__":
-    m = random.choice([hewan, komputer_ajaib])
+    m = random.choice([hewan, komputer_ajaib, zeus_olympia])
     m.madlib()
