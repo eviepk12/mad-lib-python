dcd78e32c603d2014f83f93d9e9663f52af7cfd5
Evieepk12 <eviepk12@gmail.com>
2022-02-02 20:26:20 +0700

    new madlib

---
+++ sample_madlibs/zeus_olympia.py
@@ -0,0 +1,22 @@
+def madlib():
+    kataBenda = input("Kata Benda : ")
+    emosi = input("Emosi : ")
+    anggotaKeluarga = input("Anggota Keluarga : ")
+    katakerja = input("Kata Kerja : ")
+    nomor = input("Nomor : ")
+    tempat = input("Tempat : ")
+
+    print("--------------------------------------------")
+
+    madlib = f"Sang {kataBenda} para olympia, Zeus sangatlah {emosi} dengan {anggotaKeluarga}, Kronos, karena dia pernah memakan Zeus \
+dan semua saudaranya dan {anggotaKeluarga}. Dan Kronos sangat {emosi} dengan Zeus dan \
+{anggotaKeluarga} dan saudari karena mereka harus {katakerja} dari dia. Zeus dan Kronos menginginkan \
+untuk menguasai {tempat}, tapi hanya {nomor} dari mereka yang bisa. Jadi ada pertengkaran! Zeus dan \
+saudaranya di sisinya, melawan Kronos dan para Titannya. Dua sisi sama kuatnya di {katakerja}, \
+sampai Zeus berjalan ke {tempat} and mendapat lagi {kataBenda} dary Cyclops. \
+Habis itu dia dan {anggotaKeluarga} dia dan saudarinya bisa {katakerja} ke ayah mereka dan \
+menguasai dunia dengan {emosi}"
+
+    print(madlib)
+
+madlib()
